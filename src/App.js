89e2870065c8c54e247b6be8bb09771599cf2ae1
Aadarshbhushan-Singh <singhaadarsh5050@gmail.com>
2022-06-17 11:33:43 +0530

    Added Particle.js as background of landing page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ParticleBackground from './Components/Background/ParticleBackground.js';
+import { CentreText } from './Components/Centre/CentreText';
+
+function App() {
+  return (
+    <div>
+      <ParticleBackground/>
+      <CentreText/>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/Components/Centre/CentreText.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './CentreText.css'
+
+export const CentreText = () => {
+  return (
+    <div className='centre_text'>CentreText</div>
+  )
+}
